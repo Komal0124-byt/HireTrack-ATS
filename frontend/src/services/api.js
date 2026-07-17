12902d16c5d354ba3d1e52ca559727e98d2053eb
Komal <komallamba993@gmail.com>
2026-07-17 21:16:11 +0530

    Connect frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://hiretrack-ats-xgr3.onrender.com/api",
 });
 
 export default api;
